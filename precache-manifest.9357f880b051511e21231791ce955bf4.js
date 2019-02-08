f1240083692f299fd48483f143042e719abdc7ce
Konstantinov Innokentii <konstantinovksh@gmail.com>
2019-02-09 00:58:47 +0500

    Updates

---
+++ precache-manifest.9357f880b051511e21231791ce955bf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4da0b0f9dedcea85a9ba",
+    "url": "/react-memory-card/static/js/runtime~main.4da0b0f9.js"
+  },
+  {
+    "revision": "264d841388c0fe2ff68e",
+    "url": "/react-memory-card/static/js/main.264d8413.chunk.js"
+  },
+  {
+    "revision": "6e8079d8ded2d15b273b",
+    "url": "/react-memory-card/static/js/1.6e8079d8.chunk.js"
+  },
+  {
+    "revision": "264d841388c0fe2ff68e",
+    "url": "/react-memory-card/static/css/main.c4d899e7.chunk.css"
+  },
+  {
+    "revision": "3c8d48c9e157556793ea90f82837c036",
+    "url": "/react-memory-card/index.html"
+  }
+];
