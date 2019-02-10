5e28e992662a1525b31c820bb5e6eb37141da9cb
Konstantinov Innokentii <konstantinovksh@gmail.com>
2019-02-10 22:37:26 +0500

    Updates

---
+++ precache-manifest.7e29cf608996f90a20f4e0a1c9a908e6.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "2d3e69a985011b6d8baa",
+    "url": "/react-memory-card/static/css/main.3478d314.chunk.css"
+  },
+  {
+    "revision": "2d3e69a985011b6d8baa",
+    "url": "/react-memory-card/static/js/main.2d3e69a9.chunk.js"
+  },
+  {
+    "revision": "cb8c879a8d6102e35bdf",
+    "url": "/react-memory-card/static/js/1.cb8c879a.chunk.js"
+  },
+  {
+    "revision": "4da0b0f9dedcea85a9ba",
+    "url": "/react-memory-card/static/js/runtime~main.4da0b0f9.js"
+  },
+  {
+    "revision": "ee0fa79dbfe7f510612214fe59c15553",
+    "url": "/react-memory-card/static/media/Back.ee0fa79d.png"
+  },
+  {
+    "revision": "4b8b1766ddc4e863a2cc14515f3ac0ab",
+    "url": "/react-memory-card/static/media/10C.4b8b1766.png"
+  },
+  {
+    "revision": "2b1024bed0d766b32f4bd8e50a892d47",
+    "url": "/react-memory-card/static/media/10D.2b1024be.png"
+  },
+  {
+    "revision": "0df35df482ce13edaabeae06f3fe4db7",
+    "url": "/react-memory-card/static/media/10H.0df35df4.png"
+  },
+  {
+    "revision": "691685e5500df5705dcccbbfebf4dd62",
+    "url": "/react-memory-card/static/media/10S.691685e5.png"
+  },
+  {
+    "revision": "cc35bbb7d7c0b0b369304349e947ea04",
+    "url": "/react-memory-card/static/media/11C.cc35bbb7.png"
+  },
+  {
+    "revision": "627595585b47e627ed0312c91e9ca950",
+    "url": "/react-memory-card/static/media/11D.62759558.png"
+  },
+  {
+    "revision": "00fce2b9e961bb30f171ec2562e4782f",
+    "url": "/react-memory-card/static/media/11H.00fce2b9.png"
+  },
+  {
+    "revision": "e8642bf77b0d0e2b62d9943a133efbc3",
+    "url": "/react-memory-card/static/media/11S.e8642bf7.png"
+  },
+  {
+    "revision": "af3e1c110341c06cac6713ed2889c7e6",
+    "url": "/react-memory-card/static/media/12C.af3e1c11.png"
+  },
+  {
+    "revision": "2df2193597c07054e836007608514725",
+    "url": "/react-memory-card/static/media/12D.2df21935.png"
+  },
+  {
+    "revision": "97bab12f24d6072753da4ca0b81baa23",
+    "url": "/react-memory-card/static/media/12H.97bab12f.png"
+  },
+  {
+    "revision": "b91ec0a2a154b88bee31c63b91da7e35",
+    "url": "/react-memory-card/static/media/12S.b91ec0a2.png"
+  },
+  {
+    "revision": "247ffd29154b48fe7408897e65dc8a4c",
+    "url": "/react-memory-card/static/media/13C.247ffd29.png"
+  },
+  {
+    "revision": "e7570305a1aa95c48f490c47f741ea01",
+    "url": "/react-memory-card/static/media/13D.e7570305.png"
+  },
+  {
+    "revision": "2f61b2867fc5df85e24e0bbe0692125f",
+    "url": "/react-memory-card/static/media/13H.2f61b286.png"
+  },
+  {
+    "revision": "843f3d93735fd30ba0699ce2e6ad1385",
+    "url": "/react-memory-card/static/media/13S.843f3d93.png"
+  },
+  {
+    "revision": "de3d4e06f84033ec1988aaf1467cce24",
+    "url": "/react-memory-card/static/media/5C.de3d4e06.png"
+  },
+  {
+    "revision": "bddbea02a2211da87aa8c8dad696518c",
+    "url": "/react-memory-card/static/media/5D.bddbea02.png"
+  },
+  {
+    "revision": "3b9435f5eb0b4c5880ee412f8d863dab",
+    "url": "/react-memory-card/static/media/5H.3b9435f5.png"
+  },
+  {
+    "revision": "39fe29330e6c34d06c2210784f76648b",
+    "url": "/react-memory-card/static/media/5S.39fe2933.png"
+  },
+  {
+    "revision": "c5d5f419fdf69aaabd8ecec72e75ffb3",
+    "url": "/react-memory-card/static/media/6C.c5d5f419.png"
+  },
+  {
+    "revision": "ff4cf87df8c9456d32e92c95d904b2fd",
+    "url": "/react-memory-card/static/media/6D.ff4cf87d.png"
+  },
+  {
+    "revision": "3390eb542ffb6795a048da372c871106",
+    "url": "/react-memory-card/static/media/6H.3390eb54.png"
+  },
+  {
+    "revision": "192df24353c5e37fdfe40f88af332f41",
+    "url": "/react-memory-card/static/media/6S.192df243.png"
+  },
+  {
+    "revision": "0097c04ba3f8c9e4662efb0d74b5533b",
+    "url": "/react-memory-card/static/media/7C.0097c04b.png"
+  },
+  {
+    "revision": "02156b898ec20cd989ba093f765cf9bb",
+    "url": "/react-memory-card/static/media/7D.02156b89.png"
+  },
+  {
+    "revision": "49ad39f7f05e4b41491e2251d9b6774d",
+    "url": "/react-memory-card/static/media/7H.49ad39f7.png"
+  },
+  {
+    "revision": "b2028020b98ba71e3937eb42073abe15",
+    "url": "/react-memory-card/static/media/7S.b2028020.png"
+  },
+  {
+    "revision": "f665875c756d4095319cb2e2244e43f0",
+    "url": "/react-memory-card/static/media/8C.f665875c.png"
+  },
+  {
+    "revision": "a0762887fff057f2c45eba5cbe763269",
+    "url": "/react-memory-card/static/media/8D.a0762887.png"
+  },
+  {
+    "revision": "d921c2288a96a8a78369ddb3c11aa2a8",
+    "url": "/react-memory-card/static/media/8H.d921c228.png"
+  },
+  {
+    "revision": "ad4b98d8d4ae9eab2da5daf7a8f4b989",
+    "url": "/react-memory-card/static/media/8S.ad4b98d8.png"
+  },
+  {
+    "revision": "3c828767dd632861dc7f8edb4a86fb17",
+    "url": "/react-memory-card/static/media/9C.3c828767.png"
+  },
+  {
+    "revision": "24353f4c8a10ea37e29c43c800c3639d",
+    "url": "/react-memory-card/static/media/9D.24353f4c.png"
+  },
+  {
+    "revision": "46125755f14dcef330a5f74a575afd1b",
+    "url": "/react-memory-card/static/media/9H.46125755.png"
+  },
+  {
+    "revision": "155901cd40c3327ff7ca9756d7b92bc6",
+    "url": "/react-memory-card/static/media/9S.155901cd.png"
+  },
+  {
+    "revision": "a6659f456603b909d7c4fe961697630c",
+    "url": "/react-memory-card/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-memory-card/precache-manifest.74ce047c3bfcec61659c9881ff14f7ef.js"
+  "/react-memory-card/precache-manifest.7e29cf608996f90a20f4e0a1c9a908e6.js"
 );
 
 workbox.clientsClaim();
